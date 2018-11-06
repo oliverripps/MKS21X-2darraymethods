82a179f5f760761ad5be91c4ac14fb2b7ccbf08b
Oliver Ripps <oripps@stuy.edu>
2018-11-06 11:37:08 -0500

    all functions drafted

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -17,10 +17,41 @@ public class ArrayMethods{
           }
       }
       return x;
-  }
-
-
-
-
-
+    }
+  public static int[] allRowSums(int[][] ary) {
+    int[] s = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      s[i] = rowSum(ary, i);
+        }
+    return s;
+    }
+  public static int[] allColSums(int[][] ary) {
+    int max = 0;
+    for (int i = 0; i < ary.length; i++) {
+      if (ary[i].length > max) {
+        max = ary[i].length;
+            }
+        }
+    int[] s = new int[max];
+    for (int i = 0; i < max; i++){
+      s[i] = columnSum(ary, i);
+        }
+    return s;
+    }
+    public static boolean isRowMagic(int[][] ary){
+      int[] rows = allRowSums(ary);
+      for (int i = 1; i < rows.length; i++){
+        if (rows[i] != rows[i - 1]) {
+          return false;
+        }}
+      return true;
+    }
+    public static boolean isColumnMagic(int[][] ary){
+        int[] columns = allColSums(ary);
+        for (int i = 1; i < columns.length; i++) {
+            if (columns[i] != columns[i - 1]) {
+                return false;
+            }}
+        return true;
+    }
 }
