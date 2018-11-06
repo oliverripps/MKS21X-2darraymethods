f30ee4593f232efada797f24316efd9bc3fbcbe1
Oliver Ripps <oripps@stuy.edu>
2018-11-06 11:18:16 -0500

    first function

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,13 @@
 public class ArrayMethods{
   public static int rowSum(int[][] ary, int c)
-
+  int x =0;
+  if (ary.length<=c || c<0){
+    return 0;
+  }
+  for(int i=0; i<ary[c].length;i++){
+    x+=ary[c][i];
+  }
+  return c;}
 
 
 
