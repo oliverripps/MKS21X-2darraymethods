41ce189a8d9f56a652c5136b256db7555ae88588
Oliver Ripps <oripps@stuy.edu>
2018-11-06 11:24:34 -0500

    second function

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,13 +1,23 @@
 public class ArrayMethods{
-  public static int rowSum(int[][] ary, int c)
-  int x =0;
-  if (ary.length<=c || c<0){
+  public static int rowSum(int[][] ary, int c){
+  int x=0;
+  if(ary.length<=c || c<0){
     return 0;
   }
   for(int i=0; i<ary[c].length;i++){
     x+=ary[c][i];
   }
-  return c;}
+  return x;
+}
+  public static int columnSum(int[][] ary, int c) {
+      int x = 0;
+      for (int i = 0; i < ary.length; i++) {
+          if (c < ary[i].length) {
+              x += ary[i][c];
+          }
+      }
+      return x;
+  }
 
 
 
